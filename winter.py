c2bc45ffce59143e38d026a25e81d8d960732f99
LyHoangKhangDVL <121933718+LyHoangKhangDVL@users.noreply.github.com>
2024-04-26 22:14:09 +0700

    Update winter.py

--- winter.py
+++ winter.py
@@ -1,6 +1,6 @@
 if "/".join(__import__('sys').argv[0].split("\\")).split("/")[-1] != "winter.py":
     __import__('sys')
-    print("\033[1;34m[ Winter ] Update tên tool !!!!")
+    print("\033[1;34m[ Winter ] CÁI ĐỊT MẸ MÀY !!!!")
     print("\033[1;34m[ Winter ] Đổi tên tool thành winter.py để sử dụng !!")
     exit(1)
 try:
